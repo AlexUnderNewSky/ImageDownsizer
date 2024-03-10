6218f200a746ae35842ce6e11a896f31d3d6a4c7
Serhii Sosnytskyi <55388412+AlexUnderNewSky@users.noreply.github.com>
2024-03-10 18:53:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            browseButton = new Button();
+            originalPictureBox = new PictureBox();
+            downsizeButton = new Button();
+            downscaledPictureBox = new PictureBox();
+            scaleTextBox = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)originalPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)downscaledPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // browseButton
+            // 
+            browseButton.Location = new Point(41, 327);
+            browseButton.Name = "browseButton";
+            browseButton.Size = new Size(179, 89);
+            browseButton.TabIndex = 3;
+            browseButton.Text = "Browse";
+            browseButton.UseVisualStyleBackColor = true;
+            browseButton.Click += browseButton_Click;
+            // 
+            // originalPictureBox
+            // 
+            originalPictureBox.BorderStyle = BorderStyle.FixedSingle;
+            originalPictureBox.Location = new Point(12, 12);
+            originalPictureBox.Name = "originalPictureBox";
+            originalPictureBox.Size = new Size(380, 271);
+            originalPictureBox.TabIndex = 4;
+            originalPictureBox.TabStop = false;
+            // 
+            // downsizeButton
+            // 
+            downsizeButton.Location = new Point(583, 327);
+            downsizeButton.Name = "downsizeButton";
+            downsizeButton.Size = new Size(179, 89);
+            downsizeButton.TabIndex = 5;
+            downsizeButton.Text = "Downsize";
+            downsizeButton.UseVisualStyleBackColor = true;
+            downsizeButton.Click += downsizeButton_Click;
+            // 
+            // downscaledPictureBox
+            // 
+            downscaledPictureBox.BorderStyle = BorderStyle.FixedSingle;
+            downscaledPictureBox.Location = new Point(408, 12);
+            downscaledPictureBox.Name = "downscaledPictureBox";
+            downscaledPictureBox.Size = new Size(380, 271);
+            downscaledPictureBox.TabIndex = 6;
+            downscaledPictureBox.TabStop = false;
+            // 
+            // scaleTextBox
+            // 
+            scaleTextBox.Location = new Point(232, 370);
+            scaleTextBox.Name = "scaleTextBox";
+            scaleTextBox.Size = new Size(176, 23);
+            scaleTextBox.TabIndex = 7;
+            scaleTextBox.TextChanged += scaleTextBox_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.Location = new Point(226, 347);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Please enter scaling factor:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(scaleTextBox);
+            Controls.Add(downscaledPictureBox);
+            Controls.Add(downsizeButton);
+            Controls.Add(originalPictureBox);
+            Controls.Add(browseButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)originalPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)downscaledPictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button browseButton;
+        private PictureBox originalPictureBox;
+        private Button downsizeButton;
+        private PictureBox downscaledPictureBox;
+        private TextBox scaleTextBox;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif";
+                openFileDialog.Title = "Select an image file";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        originalImage = new Bitmap(openFileDialog.FileName);
+                        originalPictureBox.Image = originalImage;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading the image: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void downsizeButton_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+
+            if (!double.TryParse(scaleTextBox.Text, out double scaleFactor) || scaleFactor <= 0 || scaleFactor > 100)
+            {
+                MessageBox.Show("Please enter a valid scaling factor (1-100).");
+                return;
+            }
+
+            int newWidth = (int)(originalImage.Width * scaleFactor / 100);
+            int newHeight = (int)(originalImage.Height * scaleFactor / 100);
+
+            Bitmap downscaledImage = DownscaleImage(originalImage, newWidth, newHeight);
+
+            downscaledPictureBox.Image = downscaledImage;
+        }
+
+        private Bitmap DownscaleImage(Bitmap original, int newWidth, int newHeight)
+        {
+            Bitmap downscaledImage = new Bitmap(newWidth, newHeight);
+
+            BitmapData originalData = original.LockBits(new Rectangle(0, 0, original.Width, original.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData downscaledData = downscaledImage.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* originalScan0 = (byte*)originalData.Scan0;
+                byte* downscaledScan0 = (byte*)downscaledData.Scan0;
+
+                for (int y = 0; y < newHeight; y++)
+                {
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        int originalX = (int)(x * original.Width / (float)newWidth);
+                        int originalY = (int)(y * original.Height / (float)newHeight);
+
+                        byte* originalPixel = originalScan0 + originalY * originalData.Stride + originalX * 3;
+                        byte* downscaledPixel = downscaledScan0 + y * downscaledData.Stride + x * 3;
+
+                        downscaledPixel[0] = originalPixel[0]; // Blue
+                        downscaledPixel[1] = originalPixel[1]; // Green
+                        downscaledPixel[2] = originalPixel[2]; // Red
+                    }
+                }
+            }
+
+            original.UnlockBits(originalData);
+            downscaledImage.UnlockBits(downscaledData);
+
+            return downscaledImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scaleTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
